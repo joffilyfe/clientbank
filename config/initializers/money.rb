bfb39fcedb1051affd1b8ed8dc6a2753905a480e
Joffily Ferreira <joffilyferreira@gmail.com>
2024-02-08 13:35:24 +0000

    Adds Money initializer
    This initializer sets how rails/money gem will handle money.
    
    By default we're going to use Euros as currency without Banks
    for exchange rates.

---
+++ config/initializers/money.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+MoneyRails.configure do |config|
+  config.default_currency = :eur
+  config.rounding_mode = BigDecimal::ROUND_HALF_UP
+  config.locale_backend = :i18n
+
+  I18n.locale = :en
+end
